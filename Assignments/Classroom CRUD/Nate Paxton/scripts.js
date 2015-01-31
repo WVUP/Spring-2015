5b1b25c3140dba1885db7c91c06ca906613989a7
Nate Paxton <natepaxton@gmail.com>
2015-01-31 12:06:27 -0500

    Form area/layout changes, JS for populating table

--- Assignments/Classroom CRUD/Nate Paxton/scripts.js
+++ Assignments/Classroom CRUD/Nate Paxton/scripts.js
@@ -3,9 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.querySelector('#begin').style.display = "block";
   document.querySelector('#main').style.display = "none";
+
+  //Populate the current students table
+  updateCurrent();
+
   logTimer("Document ready");
 });
 
+//Standalone functions
 var began = function(){
 	fadeOut(document.querySelector('#begin'));
 	document.querySelector('#begin').style.display = "none";
@@ -14,7 +19,20 @@ var began = function(){
 	logTimer("Begin screen closed, main displayed");
 }
 
-//Helper methods
+
+//Updates the Students table
+var updateCurrent = function(){
+for(var i=0; i<Students.length; i++){
+	var row = "<tr><td>" +
+	Students[i].firstName + " " + Students[i].lastName +
+	"</td><td>" + Students[i].year +
+	"</td><td>" + Students[i].major +
+	"</td></tr>";
+	document.querySelector('#currentStudents').innerHTML += row;
+	logTimer(Students[i].firstName + " " + Students[i].lastName +
+		" added to current students table");
+}}
+
 var logTimer = function(message){
 	var today = new Date();
 	console.log(message + ": " + today.toDateString() + " @ " + today.toTimeString());
@@ -43,6 +61,24 @@ var Students = new Array();
 Students.push(new Student(
 	"Nate",
 	"Paxton",
+	"Software Engineering",
+	"Senior",
+	"9.9",
+	"Charles Almond"
+))
+
+Students.push(new Student(
+	"Raylan",
+	"Givens",
+	"Criminal Justice",
+	"Junior",
+	"5.4",
+	"Rhett Wharton"
+))
+
+Students.push(new Student(
+	"Mitchell",
+	"Adams",
 	"Software Engineering",
 	"Senior",
 	"9.9",
