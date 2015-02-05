10907603386971555a07b2de6b918db970a29ce7
DustinFarnsworth <dfarnswo@wvup.edu>
2015-02-05 17:47:55 -0500

    test

---
+++ Assignments/Classroom CRUD/DF2/scripts.js
@@ -0,0 +1,45 @@
+var stu = [];
+
+function addStudent(){
+	stu = [];
+
+	stu.push(document.getElementById("full").value);
+	stu.push(document.getElementById("email").value);
+	stu.push(document.getElementById("major").value);
+	stu.push(document.getElementById("advisor").value);
+
+	var newElement = document.createElement("div");
+
+	newElement.className = "student";
+
+	newElement.appendChild(document.createTextNode(stu));
+
+	document.getElementById("enrollment").appendChild(newElement);
+
+	document.getElementById("full").value = '';
+	document.getElementById("email").value = '';
+	document.getElementById("major").value = '';
+	document.getElementById("advisor").value = '';
+}
+
+
+/*
+var students = [];
+
+function listStudents(){
+	var html = '';
+
+	for(var i = 0; i < students.length; i++){
+		studentElement +=
+		'<div>' + 
+	}
+}
+
+function addStudent(){
+	var newStudent = document.getElementById("studen").value;
+	var newElement = document.createElement("div");
+	newElement.className = "student";
+	newElement.appendChild(document.createTextNode(students));
+	document.getElementById("enrollment").appendChild(newElement);
+}
+*/
