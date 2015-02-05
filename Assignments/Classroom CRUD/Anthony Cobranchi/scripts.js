f10f27449ec26594f17965c59e4040ce23be0f8c
ACobranchi2 <cobranca@wvup.edu>
2015-02-04 22:12:08 -0500

    Starting point

---
+++ Assignments/Classroom CRUD/Anthony Cobranchi/scripts.js
@@ -0,0 +1,16 @@
+var listOfStudents =[];
+
+function addStudent(){
+	var student - new Object(); 
+	student.FirstName = document.getElementById("firstName").value;
+	student.LastName = document.getElementById("lastName").value;
+	student.Age = document.getElementById("age").value;
+	student.Email = document.getElementById("email").value;
+	student.Major = document.getElementById("major").value;
+	listOfStudents.push(student)
+
+	var i;
+	for (var i = 0; i < listOfStudents.length; i++) {
+		
+	};
+}
