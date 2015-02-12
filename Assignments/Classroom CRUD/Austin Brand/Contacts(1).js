144dd26b3c594243de925b0bee41b594db50f064
austinbrand <abrand@wvup.edu>
2015-02-12 16:34:50 -0500

    just checking in to make sure it it up to date

--- Assignments/Classroom CRUD/Austin Brand/Contacts(1).js
+++ Assignments/Classroom CRUD/Austin Brand/Contacts(1).js
@@ -84,3 +84,41 @@ function myFunctionS()
 {
 	document.getElementById("form1").submit();
 }
+
+
+
+var firstName=new Array();
+var lastName=new Array();
+var email=new Array();
+var major=new Array();
+var GPA=new Array();
+var classYear=new Array();
+
+function insert(){
+    var firstNameValue = document.getElementById('firstName').value;
+    var lastNameValue = document.getElementById('lastName').value;
+    var majorValue = document.getElementById('email').value;
+    var GPAValue = document.getElementById('GPA').value;
+    var classYearValue = document.getElementById('classYear').value;
+
+    firstName[firstName.length]=firstNameValue;
+    lastName[lastName.length]=lastNameValue;
+    major[major.length]=majorValue;
+    GPA[GPA.length]=GPAValue;
+    classYear[classYear.length]=classYear;
+  }
+
+function show() 
+{
+  var content="<b> Students: </b><br>";
+  for(var i = 0; i < firstName.length; i++) {
+     content +=firstName[i]+"<br>";
+  }
+  for(var i = 0; i < lastName.length; i++) {
+     content +=lastName[i]+"<br>";
+  }
+  for(var i = 0; i < major.length; i++) {
+     content +=major[i]+"<br>";
+  }
+  document.getElementById('display').innerHTML = content;
+}
