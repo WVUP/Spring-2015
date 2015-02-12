3368ce36523a1ab8b4c01fb268b0cc0d5ce832e7
mkashem <mkashem@wvup.edu>
2015-02-12 14:08:31 -0500

    Revert "displaying dynamic table"
    This reverts commit a859261ed3277299ab3236e02191f55b17550680.

--- Assignments/Classroom CRUD/Shane Hinzman/assignment 1/assignment1.js
+++ Assignments/Classroom CRUD/Shane Hinzman/assignment 1/assignment1.js
@@ -1,33 +1,18 @@
 var students = [];
 
-function addRow() {
+function add(){
 	var student = new Object();
 	student.fName = document.getElementById("firstName").value;
 	student.lName = document.getElementById("lastName").value;
 	student.email = document.getElementById("email").value;
 	students.push(student);
-	for (var i = 0; i < students.length; i++) {
-	    var table = document.getElementById("myTableData");
-	 
-	    var rowCount = table.rows.length;
-	    var row = table.insertRow(rowCount);
-	 
-	    row.insertCell(0).innerHTML= '';
-	    row.insertCell(1).innerHTML= students[i].fName
-	    row.insertCell(2).innerHTML= students[i].lName
-	    row.insertCell(3).innerHTML= students[i].email
-	};
 	console.log(students);
-         
- 
+	alert("First name: " + student.fName + "    Last name: " + student.lName +"    Email: " + student.email);
 }
 function deleteStudent(){
 	for (var i = 0; i < students.length; i++) {
 		if (document.getElementById("delete").value == students[i].email){
 			students.splice(i, 1);
-			    var index = students[(i + 1)];
-			    var table = document.getElementById("myTableData");
-			    table.deleteRow(index);
 		}
 	};
 	console.log(students);
