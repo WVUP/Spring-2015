9618011be5bdd126ac3c9ea0974a5418a9463080
Nate P <natepaxton@gmail.com>
2015-03-05 00:03:52 -0500

    JS, CSS, index updated - dashboard ready for storyboarding

--- Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/main.js
+++ Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/main.js
@@ -1,3 +1,18 @@
+$('#courseStartDatePicker input').datepicker({
+
+});
+$('#courseEndDatePicker input').datepicker({
+
+});
+$('#courseEndDatePicker input').datepicker({
+
+});
+$('#assignmentOpenDatePicker input').datepicker({
+
+});
+$('#assingmentCloseDatePicker input').datepicker({
+
+});
 ;(function(){
 
 			// Menu settings
@@ -9,3 +24,34 @@
 
 
 })(jQuery)
+
+var currentSemesterStudentData = [
+	{
+		value: 12,
+		color: #68dff0,
+		label: "CS 329"
+	},
+	{
+		value: 23,
+		color: #ffd777,
+		label: "CS 330"
+	},
+	{
+		value: 11,
+		color: #ff865c,
+		label: "CS 331"
+	},
+	{
+		value: 9,
+		color: #43b1a9,
+		label: "CS 332"
+	},
+	{
+		value: 4,
+		color: #797979,
+		label: "CS 333"
+	}
+]
+
+var ctx = document.getElementById("#currentBreakdown").getContext("2d");
+var currentChart = new Chart(ctx).Doughnut(currentSemesterStudentData);
