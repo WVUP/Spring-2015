741cc8ddeb679944a1d4d14fe411d79dd2a72f24
Nate P <natepaxton@gmail.com>
2015-03-05 16:20:58 -0500

    Fixed layout errors in dashboard, created doughnut chart

--- Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/main.js
+++ Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/main.js
@@ -10,9 +10,11 @@ $('#courseEndDatePicker input').datepicker({
 $('#assignmentOpenDatePicker input').datepicker({
 
 });
-$('#assingmentCloseDatePicker input').datepicker({
+$('#assignmentCloseDatePicker input').datepicker({
 
 });
+
+
 ;(function(){
 
 			// Menu settings
@@ -28,30 +30,35 @@ $('#assingmentCloseDatePicker input').datepicker({
 var currentSemesterStudentData = [
 	{
 		value: 12,
-		color: #68dff0,
+		color: '#68dff0',
+		highlight: '#fff',
 		label: "CS 329"
 	},
 	{
 		value: 23,
-		color: #ffd777,
+		color: '#ffd777',
+		highlight: '#fff',
 		label: "CS 330"
 	},
 	{
 		value: 11,
-		color: #ff865c,
+		color: '#ff865c',
+		highlight: '#fff',
 		label: "CS 331"
 	},
 	{
 		value: 9,
-		color: #43b1a9,
+		color: '#43b1a9',
+		highlight: '#fff',
 		label: "CS 332"
 	},
 	{
 		value: 4,
-		color: #797979,
+		color: '#797979',
+		highlight: '#fff',
 		label: "CS 333"
 	}
 ]
 
-var ctx = document.getElementById("#currentBreakdown").getContext("2d");
-var currentChart = new Chart(ctx).Doughnut(currentSemesterStudentData);
+var ctx = document.getElementById("currentBreakdown").getContext("2d");
+var currentChart = new Chart(ctx).Doughnut(currentSemesterStudentData, { animationEasing : "easeOutBounce", percentageInnerCutout : 65, segmentShowStroke : false });
