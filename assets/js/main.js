f641dd5624355c7edb3efd32b3c1b8669a8cd3d4
unknown <npaxton@wvup.edu>
2015-03-21 08:26:31 -0400

    Commiting to heroku remote

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+$('#courseStartDatePicker input').datepicker({
+
+});
+$('#courseEndDatePicker input').datepicker({
+
+});
+$('#courseEndDatePicker input').datepicker({
+
+});
+$('#assignmentOpenDatePicker input').datepicker({
+
+});
+$('#assignmentCloseDatePicker input').datepicker({
+
+});
+
+
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+Morris.Donut({
+	element: 'currentBreakdown',
+	data: [
+		{label: "CS 329", value: 12},
+		{label: "CS 330", value: 23},
+		{label: "CS 331", value: 11},
+		{label: "CS 332", value: 9},
+		{label: "CS 333", value: 4}
+	],
+	colors: [
+		'#ff865c',
+		'#ffd777',
+		'#43b1a9',
+		'#68dff0',
+		'#797979'
+	],
+	resize: true
+});
