0379014455b03d4636e80fe97a151e1729224f73
KevinADulaney <KevinADulaney@gmail.com>
2015-03-30 10:05:20 -0400

    student id edit
    added student id

--- Assignments/Gradebook Application/Team Anthony/group project/server.js
+++ Assignments/Gradebook Application/Team Anthony/group project/server.js
@@ -46,7 +46,7 @@ app.put('/contactlist/:id', function (req, res) {
   console.log(req.body.name);
   db.contactlist.findAndModify({
     query: {_id: mongojs.ObjectId(id)},
-    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    update: {$set: {studentid: req.body.studentid, name: req.body.name, email: req.body.email, number: req.body.number}},
     new: true}, function (err, doc) {
       res.json(doc);
     }
