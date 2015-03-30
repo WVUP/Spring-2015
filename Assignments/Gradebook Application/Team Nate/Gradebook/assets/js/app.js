fc34f9f109ebb75b1604b66bb9517759b004b248
Nate Paxton <npaxton@CTT02.wvup.edu>
2015-03-30 11:09:40 -0400

    Added angular-ui-router, updated app.js with routing

--- Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/app.js
+++ Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/app.js
@@ -1,5 +1,32 @@
-var gradebookApp = angular.module('gradebook', ['ngRoute']);
+var gradebookApp = angular.module('gradebook', ['ui.router']);
 
 gradebookApp.controller('CourseController', ['$scope', function ($scope) {
 	$scope.viewType = "Courses";
 }]);
+
+gradebookApp.factory('dataFactory', function ($http) {
+	var courseData = {content:null};
+
+	$http.get('././courses.json').success(function(data) {
+		courseData.content = data;
+	});
+
+	return courseData;
+}); 
+
+gradebookApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/courses");
+
+	$stateProvider
+		.state('courseState', {
+			url: "/courses",
+			templateUrl: "partials/courses.html"
+		})
+		.state('courseState.list', {
+			url: "courses/list",
+			templateUrl: "partials/courses.list.html",
+			controller: function($scope) {
+				$scope.courses = gradebookApp.factory;
+			}
+		});
+})
