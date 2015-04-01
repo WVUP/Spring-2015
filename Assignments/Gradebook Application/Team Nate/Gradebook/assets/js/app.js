e61d98c9291ce604f305e5be438468ba6f382ab7
Nate P <natepaxton@gmail.com>
2015-03-31 22:25:04 -0400

    Fixed graph, added semester change, modified Home controller

--- Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/app.js
+++ Assignments/Gradebook Application/Team Nate/Gradebook/assets/js/app.js
@@ -2,6 +2,25 @@ var gradebookApp = angular.module('gradebook', ['ui.router']);
 
 gradebookApp.controller('HomeController', ['$scope', function ($scope) { 
 	$scope.viewType = "Semester";
+
+	var donut = Morris.Donut({
+		element: 'currentBreakdown',
+		data: [
+			{label: "CS 329", value: 12},
+			{label: "CS 330", value: 23},
+			{label: "CS 331", value: 11},
+			{label: "CS 332", value: 9},
+			{label: "CS 333", value: 4}
+		],
+		colors: [
+			'#ff865c',
+			'#ffd777',
+			'#43b1a9',
+			'#68dff0',
+			'#797979'
+		],
+		resize: true
+	});
 }]);
 
 gradebookApp.controller('CourseController', ['$scope', function ($scope) {
@@ -30,7 +49,8 @@ gradebookApp.config(function($stateProvider, $urlRouterProvider) {
 
 		.state('courseState', {
 			url: "/courses",
-			templateUrl: "././partials/courses.html"
+			templateUrl: "././partials/courses.html",
+			controller: 'CourseController'
 		})
 
 		.state('courseState.list', {
